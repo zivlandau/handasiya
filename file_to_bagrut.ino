cc1963109fd853b50d4946353863df030d6ec6cf
zivlandau <61872971+zivlandau@users.noreply.github.com>
2020-03-06 14:12:53 +0200

    Add files via upload

---
+++ file_to_bagrut.ino
@@ -0,0 +1,34 @@
+
+#include <Servo.h> 
+Servo servo1;
+const int flexpin = 0; 
+
+void setup() 
+{ 
+  Serial.begin(9600);
+  servo1.attach(9);
+} 
+
+void loop() 
+{ 
+  int flexposition;  
+  int servoposition;
+
+  flexposition = analogRead(flexpin);
+  int N= map(flexposition, 710, 900, 180, 0);
+  
+  if  (abs(N-servoposition)>8)
+{
+  servoposition = N;
+  servoposition = constrain(servoposition, 0, 180);
+}
+
+  servo1.write(servoposition);
+
+  Serial.print("sensor: ");
+  Serial.print(flexposition);
+  Serial.print("  servo: ");
+  Serial.println(servoposition);
+
+  delay(50); 
+} 
